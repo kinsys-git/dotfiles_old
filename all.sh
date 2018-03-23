cad93a326b53de266b41502db4bbbf27be385a9c
maelodic <bradynwatson@gmail.com>
2018-03-22 18:05:42 -0700

    add fish theme install

--- all.sh
+++ all.sh
@@ -6,15 +6,14 @@ sh packages_pacman.sh
 sh packages_aur.sh
 sh bootstrap.sh
 chsh -s /usr/bin/fish
+cd ~
+mkdir tmp
+cd tmp
+echo "omf install shellder ; omf theme shellder" > fish
+fish fish
+cd ~
+rm -rf tmp/
 nitrogen --set-auto ~/dotfiles/wallpapers/526709.jpg
 nitrogen --save
 clear
 echo "All done!"
-echo ""
-echo ""
-echo "Reboot? (Y/n)"
-read reboot
-if [ "$reboot" == Y -o "$reboot" == y ]
-	then
-	reboot now
-fi
